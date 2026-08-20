f3f39cb5a512e47a049dc66b45a7854305b1642b
Pritam Pramanik <70059343+Radheya26@users.noreply.github.com>
2026-08-20 19:26:48 +0530

    Fix lists.py

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 import streamlit as st
 import lists
 
-data = {
-    "Eligible":None
-}
-
-
 def load_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
@@ -18,8 +13,8 @@ st.divider()
 
 
 with st.form(key="form"):
-    Eligible = st.checkbox("Are you Eligible?")
-    state = st.multiselect("Preferred States",lists.location_list)
+    #Eligible = st.checkbox("Are you Eligible?")
+    state = st.multiselect("Preferred Locations",lists.location_list)
     sector = st.multiselect("Target Sectors",lists.sector_list)
     field = st.multiselect("Target Fields",lists.field_list)
     skills = st.text_area("Key Skills & Interests",placeholder="e.g., Python, Java, C++, AI, Machine Learning, Accounting, Computer Networking...")
--- lists.py
+++ lists.py
@@ -2,8 +2,15 @@ import pandas as pd
 
 df = pd.read_csv('internships.csv')
 
-location_list = df["location"].dropna().unique().tolist().sort()
-sector_list = df["sector_name"].dropna().unique().tolist().sort()
-field_list = df["field_name"].dropna().unique().tolist().sort()
+location_list = df["location"].dropna().unique().tolist()
+location_list.sort()
 
+sector_list = df["sector_name"].dropna().unique().tolist()
+sector_list.sort()
 
+field_list = df["field_name"].dropna().unique().tolist()
+field_list.sort()
+
+#print(location_list)
+#print(sector_list)
+#print(field_list)
