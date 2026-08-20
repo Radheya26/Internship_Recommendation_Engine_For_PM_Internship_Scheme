621e83eaa43699dda7864720dbd340e36ea9a110
Pritam Pramanik <70059343+Radheya26@users.noreply.github.com>
2026-08-20 17:48:55 +0530

    Add Frontend

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import lists
+
+data = {
+    "Eligible":None
+}
+
+
+def load_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>",unsafe_allow_html=True)
+
+load_css("style.css")
+
+
+st.title("[ Internship Recommendation Engine ✨⚙️ ]")
+st.divider()
+
+
+with st.form(key="form"):
+    Eligible = st.checkbox("Are you Eligible?")
+    state = st.multiselect("Preferred States",lists.state_list)
+    sector = st.multiselect("Target Sectors",lists.sector_list)
+    field = st.multiselect("Target Fields",lists.field_list)
+    skills = st.text_area("Key Skills & Interests",placeholder="e.g., Python, Java, C++, AI, Machine Learning, Accounting, Computer Networking...")
+
+    submitted = st.form_submit_button("Find Internships!")
+
---
+++ src/internship_recommendation_engine_for_pm_intership_scheme/__init__.py
@@ -0,0 +1,2 @@
+def main() -> None:
+    print("Hello from internship-recommendation-engine-for-pm-intership-scheme!")
